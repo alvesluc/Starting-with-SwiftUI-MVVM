e591032d34f61a3c655e0223e752dfa2001309e0
Lucas Alves <l.alves001@gmail.com>
2022-07-18 22:54:58 -0300

    Initial Commit

---
+++ swiftui-mvvm/swiftui_mvvmApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_mvvmApp.swift
+//  swiftui-mvvm
+//
+//  Created by Lucas Alves on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
